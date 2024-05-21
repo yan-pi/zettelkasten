15055b2a260f520fdc32606e1d5f510fd8b4f2d7
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-05-20 21:53:22 -0300

    feat: start gen-rss script

---
+++ scripts/gen-rss.js
@@ -0,0 +1,39 @@
+const { promises: fs } = require('fs')
+const path = require('path')
+const RSS = require('rss')
+const matter = require('gray-matter')
+
+async function generate() {
+  const feed = new RSS({
+    title: 'Rajdeep Singh -- demo blog built with nextra and nextjs',
+    site_url: 'https://yan-zettelkasten.vercel.app/blog',
+    feed_url: 'https://yan-zettelkasten.vercel.app/blog/rss'
+  })
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+
+  await Promise.all(
+    posts.map(async (name) => {
+      if (name.startsWith('index.')) return
+
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'pages', 'posts', name)
+      )
+      const frontmatter = matter(content)
+
+
+      feed.item({
+        title: frontmatter.data.title,
+        url: '/posts/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.date,
+        description: frontmatter.data.description,
+        categories: frontmatter.data.tag.split(','),
+        author: frontmatter.data.author
+      })
+    })
+  )
+
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+}
+
+generate()
