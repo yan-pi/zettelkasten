c351f940fc3b6672a8f8cab25c09b8f57b8efe17
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-07-25 01:09:53 -0300

    feat: create rss and generate feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,32 +5,44 @@ const matter = require('gray-matter');
 
 async function generate() {
   const feed = new RSS({
-    title: 'Rajdeep Singh -- demo blog built with nextra and nextjs',
-    site_url: 'https://yan-zettelkasten.vercel.app/blog',
-    feed_url: 'https://yan-zettelkasten.vercel.app/blog/rss',
+    title: 'Yan Fernandes Blog',
+    site_url: 'https://yan-zettelkasten.vercel.app/blog/',
+    feed_url: 'https://yan-zettelkasten.vercel.app/feed.xml',
+    language: 'en',
   });
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'));
+  const getBlogPosts = async (dir) => {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(entries.map(async (entry) => {
+      const res = path.resolve(dir, entry.name);
+      return entry.isDirectory() ? getBlogPosts(res) : res;
+    }));
+    return Array.prototype.concat(...files);
+  };
+
+  const posts = await getBlogPosts(path.join(__dirname, '..', 'pages', 'blog'));
 
   await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return;
+    posts.map(async (filePath) => {
+      if (!filePath.endsWith('.md') && !filePath.endsWith('.mdx')) return;
 
-      const content = await fs.readFile(path.join(__dirname, '..', 'pages', 'posts', name));
+      const content = await fs.readFile(filePath, 'utf8');
       const frontmatter = matter(content);
 
       feed.item({
         title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
+        url: 'https://yan-zettelkasten.vercel.app/blog/' + path.basename(filePath).replace(/\.mdx?/, ''),
+        date: new Date(frontmatter.data.date).toUTCString(),
         description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(','),
+        categories: frontmatter.data.tags,
         author: frontmatter.data.author,
+        enclosure: { url: 'https://yan-zettelkasten.vercel.app' + frontmatter.data.image },
       });
     })
   );
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
+  await fs.writeFile(path.join(__dirname, '..', 'public', 'feed.xml'), feed.xml({ indent: true }));
+  console.log('RSS feed generated successfully!');
 }
 
-generate();
+generate().catch(err => console.error(err));
