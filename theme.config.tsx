11db7867be8e80e87dd19c5d4d98703a30928b4a
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-07-25 00:07:54 -0300

    feat: add ogs sidebarconfig and other configs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import { DocsThemeConfig } from 'nextra-theme-docs';
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
+import { useRouter } from 'next/router';
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+  ? `${process.env.NEXT_PUBLIC_BASE_URL}`
+  : 'https://yan-zettelkasten.vercel.app/'
 
 const config: DocsThemeConfig = {
-  logo: <h4>⚡ Yan Zettelkasten</h4>,
-  head: "Yan's Zettelkasten",
+  logo: <strong>⚡ Yan Fernandes</strong>,
   faviconGlyph: '⚡',
   useNextSeoProps() {
-    return {
-      titleTemplate: '%s - Yan Zettel',
-    };
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Yan Zettelkasten'
+      }
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      `${baseUrl}` +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'The next site builder'}
+        />
+      </>
+    )
   },
   nextThemes: {
     defaultTheme: 'dark',
@@ -16,6 +41,10 @@ const config: DocsThemeConfig = {
   primaryHue: {
     light: 29,
     dark: 29,
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
   },
   project: {
     link: 'https://github.com/yan-pi/zettelkasten/tree/main',
@@ -29,8 +58,16 @@ const config: DocsThemeConfig = {
     next: true,
   },
   footer: {
-    text: 'Made With Nextra Docs Template',
-  },
+    text: (
+      <span>
+        CC-BY-4.0 license {new Date().getFullYear()} ©{' '}
+        <a href="https://github.com/yan-pi/zettelkasten/tree/main?tab=CC-BY-4.0-1-ov-file" target="_blank">
+          Yan Fernandes
+        </a>
+        .
+      </span>
+    )
+  }
 };
 
 export default config;
