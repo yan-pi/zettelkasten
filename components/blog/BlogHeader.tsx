196cb17091da0fb0f26a84e4b847ce29a40d6c48
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-01 21:52:42 -0300

    adjust blog header and regen rss.xml feed

--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -1,8 +1,8 @@
 import { Calendar } from 'lucide-react';
 
 export const BlogHeader = ({ title, description, date }: { title: string; description?: string; date?: string }) => (
-  <div className="flex items-start flex-col gap-1 my-10">
-    <h1 className="flex font-bold leading-snug text-balance">{title}</h1>
+  <div className="flex flex-col gap-1 my-10">
+    <h1 className="font-bold leading-snug">{title}</h1>
     <div className="flex flex-row gap-4">
       <Calendar />
       <span className="text-gray-400">{date}</span>
