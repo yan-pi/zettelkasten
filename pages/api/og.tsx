dacbd2987e0d7e8a53b1a3ae6f836916ad8adaa2
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-12 17:50:03 -0300

    refactor: Update og, image URL and dependencies

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,30 +1,62 @@
-// pages/api/og.tsx
 import { ImageResponse } from '@vercel/og';
 
 export const config = {
-  runtime: 'experimental-edge',
+  runtime: 'edge',
 };
 
-export default function handler(req) {
+export default async function handler(req) {
   const { searchParams } = new URL(req.url);
+  const description = searchParams.get('description') || '';
   const title = searchParams.get('title') || 'Yan Fernandes';
 
   return new ImageResponse(
     (
       <div
         style={{
+          width: '1200px',
+          height: '630px',
+          padding: '27px 32px',
+          background: '#2B2B2B',
           display: 'flex',
-          height: '100%',
-          width: '100%',
+          justifyContent: 'space-between',
           alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: '#000',
-          color: '#fff',
-          fontSize: 60,
-          fontWeight: 'bold',
+          // fontFamily: inter.style.fontFamily,
         }}
       >
-        {title}
+        <div
+          style={{
+            width: '519px',
+            height: '124px',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            alignItems: 'flex-start',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontSize: '64px',
+              fontWeight: '700',
+              wordWrap: 'break-word',
+            }}
+          >
+            {title}
+          </div>
+          <div
+            style={{
+              color: 'white',
+              fontSize: '32px',
+              fontWeight: '500',
+              wordWrap: 'break-word',
+            }}
+          >
+            {description}
+          </div>
+        </div>
+        <div style={{ width: '128px', height: '128px' }}>
+          {/* <img src={logoUrl} alt="my logo" width="128" height="128" /> */}
+        </div>
       </div>
     ),
     {
