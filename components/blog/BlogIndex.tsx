9a66bf6f5d2c4ec4d17d8cf346cd9bc7dfaa1305
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-12 10:54:57 -0300

    chore: Update BlogIndex layout for responsive design

--- components/blog/BlogIndex.tsx
+++ components/blog/BlogIndex.tsx
@@ -6,9 +6,9 @@ import { Calendar } from 'lucide-react';
 export const BlogIndex = ({ maxItems }: { maxItems?: number }) => (
   <div>
     {(getPagesUnderRoute('/blog') as Array<Page & { frontMatter: any }>).slice(0, maxItems).map((page) => (
-      <Link key={page.route} href={page.route} className="flex justify-between items-center mb-8 group">
+      <Link key={page.route} href={page.route} className="flex flex-col md:flex-row justify-between md:items-center mb-8 group">
         <div className="flex items-center justify-between">
-          <h2 className="block font-bold text-2xl opacity-90 group-hover:opacity-100">
+          <h2 className="block font-bold text-sm opacity-90 group-hover:opacity-100 md:text-2xl">
             {page.meta?.title || page.frontMatter?.title || page.name}
           </h2>
         </div>
