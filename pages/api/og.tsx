6e0bffeefcb09d45c6d438500522f568426a7d0d
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-08-19 19:33:02 -0300

    refactor: delete font loading in og.tsx API handler

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -13,16 +13,6 @@ export default async function handler(req) {
 
   // Fetch fonts only if necessary
   const fontPromises = [];
-  if (title || tags.length > 0) {
-    fontPromises.push(
-      fetch(new URL('../../public/Inter_18pt-Bold.ttf', import.meta.url)).then((res) => res.arrayBuffer())
-    );
-  }
-  if (date) {
-    fontPromises.push(
-      fetch(new URL('../../public/Inter_18pt-Medium.ttf', import.meta.url)).then((res) => res.arrayBuffer())
-    );
-  }
   const [interBoldFontData, interMediumFontData] = await Promise.all(fontPromises);
 
   return new ImageResponse(
@@ -47,20 +37,20 @@ export default async function handler(req) {
         >
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             <p
-              style={{ fontSize: '72px', fontWeight: 700, marginTop: 4, color: 'whitesmoke', fontFamily: 'Inter-Bold' }}
+              style={{ fontSize: '72px', fontWeight: 700, marginTop: 4, color: 'whitesmoke' }}
             >
               {title}
             </p>
           </div>
           <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
             {tags.length > 0 ? (
-              <p style={{ display: 'flex', gap: 8, color: 'whitesmoke', fontFamily: 'Inter-Medium' }}>
+              <p style={{ display: 'flex', gap: 8, color: 'whitesmoke' }}>
                 {tags.map((tag, i) => (
                   <span key={i}>#{tag}</span>
                 ))}
               </p>
             ) : null}
-            {date ? <p style={{ color: 'whitesmoke', fontFamily: 'Inter-Medium' }}>{date}</p> : null}
+            {date ? <p style={{ color: 'whitesmoke' }}>{date}</p> : null}
           </div>
         </div>
       </div>
@@ -68,16 +58,6 @@ export default async function handler(req) {
     {
       width: 1200,
       height: 630,
-      fonts: [
-        {
-          name: 'Inter-Bold',
-          data: interBoldFontData,
-        },
-        {
-          name: 'Inter-Medium',
-          data: interMediumFontData,
-        },
-      ],
     }
   );
 }
