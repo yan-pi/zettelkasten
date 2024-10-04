3a3d768dee96ecc1742239ac8dc95c7c1446d2e9
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-10-04 11:44:49 -0300

    refactor: adjust blog list in index to render only first 3

---
+++ components/blog/SplitBlogIndex.tsx
@@ -0,0 +1,26 @@
+import { getPagesUnderRoute } from 'nextra/context';
+import Link from 'next/link';
+import { type Page } from 'nextra';
+import { Calendar } from 'lucide-react';
+
+export const SplitBlogIndex = () => (
+  <div>
+    {(getPagesUnderRoute('/blog') as Array<Page & { frontMatter: any }>).slice(0, 3).map((page) => (
+      <Link key={page.route} href={page.route} className="flex flex-col md:flex-row justify-between md:items-center mb-8 group">
+        <div className="flex items-center justify-between">
+          <h2 className="block font-bold text-sm opacity-90 group-hover:opacity-100 md:text-2xl">
+            {page.meta?.title || page.frontMatter?.title || page.name}
+          </h2>
+        </div>
+        <div className="flex gap-2 flex-wrap items-baseline">
+          {page.frontMatter?.date ? (
+            <span className="flex items-center gap-3 opacity-60 text-sm group-hover:opacity-100">
+              <Calendar />
+              {page.frontMatter.date}
+            </span>
+          ) : null}
+        </div>
+      </Link>
+    ))}
+  </div>
+);
